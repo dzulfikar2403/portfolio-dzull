71774d9596f94b61bf4b77ea871d910a74c19654
dzulfikar2403 <dzulfikar2403@.com>
2023-10-31 10:57:50 +0700

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,161 @@
+// original js
+window.onscroll = ()=>{
+  let nav = document.querySelector('header');
+  let fixedNav = nav.offsetTop;
+  // let sidenav = document.querySelector('#sidebar');
+  // let fixedNav2 = nav.offsetHeight;
+
+  if(window.scrollY > fixedNav ){
+    nav.classList.add('nav-fixed');
+  }else{
+    nav.classList.remove('nav-fixed');
+  }
+
+  // if(window.scrollY == fixedNav2 ){
+  //   sidenav.style.display = "block"
+  // }else{
+  //   sidenav.remove();
+  // }
+}
+
+// swipper js
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView:1,
+  speed:2000,
+  spaceBetween:15,
+  autoplay:{
+    duration:3000
+  },
+  // And if we need scrollbar
+  breakpoints:{
+    768: {
+      slidesPerView: 2,
+    }
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true
+  },
+});
+
+
+// particle js
+window.addEventListener('DOMContentLoaded',(event)=>{
+
+  particlesJS('particles-js',
+    
+    {
+      "particles": {
+        "number": {
+          "value": 40,
+          "density": {
+            "enable": true,
+            "value_area": 2700
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "shape": {
+          "type": "triangle",
+          "stroke": {
+            "width": 4,
+            "color": "#46bfd6"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.08,
+          "random": false,
+          "anim": {
+            "enable": true,
+            "speed": 3.6,
+            "opacity_min": 0.51,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 10.5,
+          "random": true,
+          "anim": {
+            "enable": true,
+            "speed": 20,
+            "size_min": 7.77,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": false,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 3.2,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "bubble"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 400,
+            "size": 20,
+            "duration": 2,
+            "opacity": 8,
+            "speed": 5
+          },
+          "repulse": {
+            "distance": 200
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 1
+          }
+        }
+      },
+      "retina_detect": true,
+    }
+  
+  );
+  })
+
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#B6FFFA",
+        footer: "#687EFF",
+        dark: "#262626",
+        secondDark:"#737373",
+        color: "#80B3FF",
+        section: "#98E4FF",
+      },
+    },
+  },
+  plugins: [],
+};
